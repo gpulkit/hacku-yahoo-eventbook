9c6ee99ffa604aeadd8a7fc7ff2525d29a2845dd
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2012-03-17 01:56:55 -0400

    change greeting

--- index.php
+++ index.php
@@ -70,17 +70,15 @@ if ($user_id) {
     'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
   ));
 
-
-
-
 /*
-  $events = $facebook->api(array('method' => 'fql.query','query' => 'SELECT eid FROM event_member WHERE uid=me()'));
-
-  $events = $facebook->api(array('method' => 'fql.query','query' => 'SELECT name, venue, location, start_time, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid=me())'));
-*/
-
   $events = $facebook->api(array('method' => 'fql.query',
 				 'query'  => 'SELECT pic_big, name, description, start_time, end_time, location, venue, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) OR uid = me())'));//  < \'88\''));//   \''.($long+$offset) .'\''));
+*/
+
+
+
+  $events = $facebook->api(array('method' => 'fql.query',
+				 'query'  => 'SELECT pic_big, name, description, start_time, end_time, location, venue, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid = me())'));//  < \'88\''));//   \''.($long+$offset) .'\''));
 
 // AND venue.longitude < \''. ($long+$offset) .'\' AND venue.latitude < \''. ($lat+$offset) .'\' AND venue.longitude > \''. ($long-$offset) .'\' AND venue.latitude > \''. ($lat-$offset) .'\' ORDER BY start_time ASC '));
 
