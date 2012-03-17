3bfc8095e3995701dd3b2cc64a7616828702a2fb
Aditya Tayade <aktayade@gmail.com>
2012-03-17 11:49:04 -0400

    commit

--- index.php
+++ index.php
@@ -227,7 +227,7 @@ function errorFunction(pos) {
 //addMarker(loc,"'.$name.'",'.$lat.','.$long.','.$pic_big.','.$description.','.$start_time.','.$end_time.');
 function addMarker(loc, ev_name, lat, lon, pic_url, desc, start_time, end_time) 
 {
-	//var img = "<img src="+pic_url+">";
+	var img = "<img src='"+pic_url+"'>";
   	marker = new google.maps.Marker({position:loc,map:map});
 	var s_d = new Date(start_time);
 	var e_d = new Date(end_time);
