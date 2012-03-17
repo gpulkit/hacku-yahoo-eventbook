8217e470f0bf4e884e21b69ac7c4f57855eb591b
Aditya Tayade <aktayade@gmail.com>
2012-03-17 07:28:13 -0400

    commit

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
-
-/**
- * This sample app is provided to kickstart your experience using Facebook's
- * resources for developers.  This sample app provides examples of several
- * key concepts, including authentication, the Graph API, and FQL (Facebook
- * Query Language). Please visit the docs at 'developers.facebook.com/docs'
- * to learn more about the resources available to you
- */
-
 // Provides access to app specific values such as your app id and app secret.
 // Defined in 'AppInfo.php'
 require_once('AppInfo.php');
-
 // Enforce https on production
-if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
-  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-  exit();
+if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') 
+{
+	header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+	exit();
 }
 
 // This provides access to helper functions defined in 'utils.php'
@@ -77,7 +68,7 @@ if ($user_id) {
 */
 
 
-  $events = $facebook->api(array('method' => 'fql.query',
+$events = $facebook->api(array('method' => 'fql.query',
 				 'query'  => 'SELECT pic_big, name, description, start_time, end_time, location, venue, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid = me())'));//  < \'88\''));//   \''.($long+$offset) .'\''));
 
 
@@ -209,7 +200,7 @@ $app_name = idx($app_info, 'name', '');
 function addMarker(loc) {
   marker = new google.maps.Marker({
     position: loc,
-    color: "#FF0000",
+    color: "#FFFFFF",
     map: map
   });
   markersArray.push(marker);
